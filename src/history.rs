4ff54dbab5522d50a2f3e2a4ec73637d800b0686
brackle <brianrackle@hotmail.com>
2020-07-08 21:42:36 -0700

    Able to convert missing to downloaded to cleaned

--- src/history.rs
+++ src/history.rs
@@ -2,8 +2,10 @@ use core::time;
 use std::borrow::{Borrow, BorrowMut};
 use std::fs::{File, OpenOptions};
 use std::io::{BufReader, BufWriter};
+use std::iter::{Filter, Map};
 use std::ops::{Deref, DerefMut};
 use std::path::Path;
+use std::slice::Iter;
 use std::thread::sleep;
 
 use serde::{Deserialize, Serialize};
@@ -26,15 +28,14 @@ struct History {
     records: Vec<Record>,
 }
 
-//TODO: store magnet
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 struct Record {
     imdb_id: String,
     title: String,
     status: State,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Copy, Clone)]
 enum State {
     //Movie not found in library
     Missing,
@@ -44,15 +45,63 @@ enum State {
     Downloaded,
     //Movie in library and cleaned
     Cleaned,
+    //TODO: get rid of downloaded state Missing or Downloading should straight to Downloaded
+    //Movie found in library that didn't exist in history
+    //Found,
 }
 
+#[derive(Copy, Clone)]
 enum ChangeOption {
     Upsert,
     Insert,
     Update,
 }
 
-//TODO: make history the manager of deluge and plex data
+//TODO: use enum to return values as they were entered into history or not
+#[derive(Clone)]
+enum Operation {
+    Modified(Record),
+    Existing(Record),
+    Invalid,
+}
+
+impl History {
+    pub fn get_records(&self, imdb_ids: &[String], predicate: impl Fn(&Record) -> bool) -> Vec<Option<&Record>> {
+        imdb_ids.iter().map(|imdb_id| self.get_record(imdb_id, &predicate)).collect()
+    }
+
+    pub fn get_records_index(&self, imdb_ids: &[String], predicate: impl Fn(&Record) -> bool) -> Vec<Option<usize>> {
+        imdb_ids.iter().map(|imdb_id| self.get_record_index(imdb_id, &predicate)).collect()
+    }
+
+    pub fn is_record_in_state(&self, imdb_id: &String, status: State, predicate: impl Fn(&Record) -> bool) -> bool {
+        match self.get_record(&imdb_id, predicate) {
+            Some(record) => std::mem::discriminant(&record.status) == std::mem::discriminant(&status),
+            None => false,
+        }
+    }
+
+    pub fn get_record(&self, imdb_id: &String, predicate: impl Fn(&Record) -> bool) -> Option<&Record> {
+        let mut found = None;
+        for record in self.records.iter() {
+            if record.imdb_id == *imdb_id {
+                if predicate(record) { found = Some(record); } else { break; }
+            }
+        }
+        found
+    }
+
+    pub fn get_record_index(&self, imdb_id: &String, predicate: impl Fn(&Record) -> bool) -> Option<usize> {
+        let mut found = None;
+        for (i, record) in self.records.iter().enumerate() {
+            if record.imdb_id == *imdb_id {
+                if predicate(record) { found = Some(i); } else { break; }
+            }
+        }
+        found
+    }
+}
+
 //TODO: remove deluge entry once movie is Downloaded
 //TODO: record time with state so stuck downloading movies can eventually be cleared
 impl MediaManager {
@@ -63,8 +112,6 @@ impl MediaManager {
         };
         let pmds = plex::get_plex_library_guids(&config).expect("Exiting (Plex GUIDs Not Found)");
         let rarbg_token = get_rarbg_token(&config).unwrap();
-        //TODO: set the state to broken if plex library does not have the title in History
-        // Movie not found in library
         let mut manager = MediaManager {
             config,
             rarbg_token,
@@ -74,6 +121,15 @@ impl MediaManager {
         manager.init_history(&pmds);
         manager.save_history();
         manager
+    }
+
+    pub fn save_history(&self) {
+        let file = OpenOptions::new()
+            .write(true).create(true).truncate(true).open(&self.config.history_file)
+            .expect(&format!("Unable to create history file {}", &self.config.history_file));
+
+        serde_json::to_writer(BufWriter::new(file), &self.history)
+            .expect(&format!("Unable to initalize history {}", &self.config.history_file));
     }
 
     //check if it exists in history and add it as downloading if it doesnt
@@ -90,18 +146,19 @@ impl MediaManager {
         let title_option = get_movie_title(&self.config, imdb_id);
         if let Some(title) = title_option {
             err_index += 1;
-            if !self.history.records.iter().any(|x| x.imdb_id == imdb_id.to_lowercase()) {
+            if self.history.get_record(&imdb_id.to_lowercase(), |_| true).is_none() {
                 err_index += 1;
                 if let Some(magnet) = rarbg::get_rarbg_magnet(&self.config, &self.rarbg_token, &imdb_id) {
                     err_index += 1;
                     deluge::add_torrent(&self.config, &magnet);
+
                     self.add_record(
                         Record {
                             imdb_id: imdb_id.to_string(),
                             title: title.clone(),
                             status: State::Downloading,
                         },
-                        ChangeOption::Insert,
+                        ChangeOption::Upsert,
                         |_, _| true);
                     if save {
                         self.save_history();
@@ -121,23 +178,22 @@ impl MediaManager {
     }
 
     pub fn clean_library(&mut self, save: bool) {
-        let mut updates = Vec::<Record>::new();
         let missing_ids: Vec<String> = self.history.records.iter()
             .filter(|record| matches!(record.status, State::Missing))
             .map(|record| record.imdb_id.clone())
             .collect();
 
         for missing_id in missing_ids {
-            let position = self.history.records.iter()
-                .position(|record| record.imdb_id == missing_id).unwrap();
+            let position = self.history.get_record_index(&missing_id, |_| true).unwrap();
             self.history.records.remove(position);
             self.add_torrent(&missing_id, false);
         }
 
-        // TODO: panic will cause unsaved history
+        let mut updates = Vec::<Record>::new();
         for record in self.history.records.iter()
             .filter(|r| matches!(r.status, State::Downloaded)) {
             if let Some(tmdb_title) = tmdb::get_movie_title(&self.config, &record.imdb_id) {
+                //TODO: modify in place instead
                 let updated_record = Record {
                     imdb_id: record.imdb_id.clone(),
                     title: tmdb_title.clone(),
@@ -145,16 +201,13 @@ impl MediaManager {
                 };
 
                 if record.title != tmdb_title {
-                    match plex::find_key_by_imdb_id(&self.config, &record.imdb_id) {
-                        Some(key) => {
-                            plex::put_plex_movie_metadata(&self.config, &key, &tmdb_title);
-                            updates.push(updated_record);
-                            println!("Updating imdb_id {} from \"{}\" to \"{}\"",
-                                     record.imdb_id,
-                                     record.title,
-                                     tmdb_title);
-                        }
-                        None => println!("Unable to update imdb_id (No rating key found) {}", record.imdb_id),
+                    if let Some(key) = plex::find_key_by_imdb_id(&self.config, &record.imdb_id) {
+                        plex::put_plex_movie_metadata(&self.config, &key, &tmdb_title);
+                        updates.push(updated_record);
+                        println!("Updating imdb_id {} from \"{}\" to \"{}\"",
+                                 record.imdb_id,
+                                 record.title,
+                                 tmdb_title);
                     }
                 } else {
                     updates.push(updated_record);
@@ -172,28 +225,87 @@ impl MediaManager {
         }
     }
 
+    //TODO: clean should be automatic during init
     // Initialize history from file or from scratch and update it with latest state
     fn init_history(&mut self, pmds: &[plex::PlexMetadata]) {
         match File::open(&self.config.history_file) {
             Ok(file) => {
                 let reader = BufReader::new(file);
-                self.history = serde_json::from_reader(reader)
-                    .expect(&format!("Unable to deserialize history {}", &self.config.history_file));
-
+                self.history = serde_json::from_reader(reader).expect(&format!("Unable to deserialize history {}", &self.config.history_file));
                 self.set_missing_records(pmds);
-                self.pmds_to_upsert(pmds)
-                    .iter()
-                    .copied()
-                    .map(MediaManager::pmd_to_record)
-                    .for_each(|record| self.upsert_record(record));
+
+                //Update non-cleaned pmds as downloaded  which then allows it to be cleaned next
+                //TODO: MAINTENANCE Can convert this to upsert...
+                self.add_pmds(pmds,
+                              ChangeOption::Update,
+                              |history, record| {
+                                  !history.is_record_in_state(&record.imdb_id, State::Cleaned, |_| true)
+                              });
+
+                //Insert new pmds as downloaded
+                self.add_pmds(pmds, ChangeOption::Insert, |_, _| true);
             }
             Err(..) => {
-                pmds
-                    .iter()
-                    .map(MediaManager::pmd_to_record)
-                    .for_each(|record| self.upsert_record(record));
+                self.add_pmds(pmds, ChangeOption::Insert, |_, _| true);
             }
         }
+    }
+
+    fn add_record(&mut self,
+                  record: Record,
+                  change_option: ChangeOption,
+                  predicate: impl Fn(&History, &Record) -> bool) -> bool {
+        let mut change = false;
+        if !record.imdb_id.is_empty() {
+            change = match change_option {
+                ChangeOption::Upsert => {
+                    self.upsert_record(record, predicate)
+                }
+                ChangeOption::Insert => {
+                    if !self.history.records.iter().any(|r| r.imdb_id == record.imdb_id) {
+                        self.upsert_record(record, predicate)
+                    } else {
+                        false
+                    }
+                }
+                ChangeOption::Update => {
+                    if self.history.records.iter().any(|r| r.imdb_id == record.imdb_id) {
+                        self.upsert_record(record, predicate)
+                    } else {
+                        false
+                    }
+                }
+            }
+        }
+        change
+    }
+
+    // Upserts if predicate returns true, or is None
+    fn upsert_record(&mut self,
+                     record: Record,
+                     predicate: impl Fn(&History, &Record) -> bool) -> bool {
+        if predicate(&self.history, &record) {
+            match self.history.records.iter_mut().find(
+                |r| r.imdb_id == record.imdb_id) {
+                Some(_record) => *_record = record,
+                None => self.history.records.push(record),
+            }
+            true
+        } else {
+            false
+        }
+    }
+    ///////////////////
+    //Utility Functions
+    ///////////////////
+
+    //adds pmds and returns the ones that were not added/updated
+    fn add_pmds(&mut self, pmds: &[plex::PlexMetadata], change_option: ChangeOption, predicate: impl Fn(&History, &Record) -> bool) -> Vec<Record> {
+        pmds.iter()
+            .map(MediaManager::pmd_to_record)
+            .filter(|record| {
+                !self.add_record(record.clone(), change_option, &predicate)
+            }).collect()
     }
 
     //Make State Missing if item is in History but not Plex library
@@ -206,80 +318,12 @@ impl MediaManager {
             .for_each(|r| r.status = State::Missing);
     }
 
-    fn add_record(&mut self,
-                  record: Record,
-                  change_option: ChangeOption,
-                  predicate: impl Fn(&History, &Record) -> bool) {
-        match change_option {
-            ChangeOption::Upsert => {
-                self.upsert_record_with_predicate(record, predicate);
-            }
-            ChangeOption::Insert => {
-                if !self.history.records.iter().any(|r| r.imdb_id == record.imdb_id) {
-                    self.upsert_record_with_predicate(record, predicate);
-                }
-            }
-            ChangeOption::Update => {
-                if self.history.records.iter().any(|r| r.imdb_id == record.imdb_id) {
-                    self.upsert_record_with_predicate(record, predicate);
-                }
-            }
-        }
-    }
-
-    pub fn save_history(&self) {
-        let file = OpenOptions::new()
-            .write(true).create(true).truncate(true).open(&self.config.history_file)
-            .expect(&format!("Unable to create history file {}", &self.config.history_file));
-
-        serde_json::to_writer(BufWriter::new(file), &self.history)
-            .expect(&format!("Unable to initalize history {}", &self.config.history_file));
-    }
-
-    // returns pmds which have an imdb_id
-    // if the imdb_id matches an existing then entry should have Downloaded or Cleaned status
-    fn pmds_to_upsert<'a>(&self, pmds: &'a [plex::PlexMetadata]) -> Vec<&'a plex::PlexMetadata> {
-        pmds.iter().filter(|pmd|
-            !pmd.imdb_guid().is_empty() &&
-                !self.history.records.iter().any(|history|
-                    history.imdb_id == pmd.imdb_guid() && (
-                        matches!(history.status, State::Downloaded) || matches!(history.status, State::Cleaned)
-                    )
-                )).collect()
-    }
-
     //convert a pmd to a movie record. If the pmd exists as downloading, convert it to cleaned
     fn pmd_to_record(pmd: &PlexMetadata) -> Record {
-        //TODO: create functions to do basic operations like any
-        //        Record {
-        //             imdb_id: pmd.imdb_guid(),
-        //             title: pmd.title.clone(),
-        //             status: match self
-        //                 .history.records.iter().any(|record| record.imdb_id == pmd.imdb_guid() && matches!(record.status, State::Downloading)) {
-        //                 true => matched,
-        //                 false => unmatched,
-        //             },
         Record {
             imdb_id: pmd.imdb_guid(),
             title: pmd.title.clone(),
             status: State::Downloaded,
-        }
-    }
-
-    // Upserts if predicate returns true, or is None
-    fn upsert_record_with_predicate(&mut self,
-                                    record: Record,
-                                    predicate: impl Fn(&History, &Record) -> bool) {
-        if predicate(&self.history, &record) {
-            self.upsert_record(record);
-        }
-    }
-
-    fn upsert_record(&mut self, record: Record) {
-        match self.history.records.iter_mut().find(
-            |r| r.imdb_id == record.imdb_id) {
-            Some(_record) => *_record = record,
-            None => self.history.records.push(record),
         }
     }
 }
@@ -308,10 +352,11 @@ mod test {
                 min_imdb_rating: 0,
                 tmdb_v4_api_key: "".to_string(),
             },
-            history: History { records: vec!(
-                    Record { imdb_id: "tt0021749".into(), title: "in pmds".into(), status: State::Cleaned},
-                    Record { imdb_id: "tt0597723552".into(), title: "not in pmds and no magnet".into(), status: State::Cleaned},
-                    Record { imdb_id: "tt0023984212".into(), title: "not in pmds and no magnet and missing".into(), status: State::Missing})
+            history: History {
+                records: vec!(
+                    Record { imdb_id: "tt0021749".into(), title: "in pmds".into(), status: State::Cleaned },
+                    Record { imdb_id: "tt0597723552".into(), title: "not in pmds and no magnet".into(), status: State::Cleaned },
+                    Record { imdb_id: "tt0023984212".into(), title: "not in pmds and no magnet and missing".into(), status: State::Missing })
             },
             rarbg_token: "".into(),
         }
@@ -347,10 +392,12 @@ mod test {
 
     #[test]
     fn test_pmds_to_upsert() {
-        let manager = media_manager();
-        let pmds = plex_metadata();
-        let upserts = manager.pmds_to_upsert(&pmds);
-        assert_eq!(upserts.len(), 2);
+        assert!(false);
+
+        // let manager = media_manager();
+        // let pmds = plex_metadata();
+        //let upserts = manager.get_new_pmds(&pmds);
+        //assert_eq!(upserts.len(), 2);
     }
 
     #[test]
@@ -361,28 +408,30 @@ mod test {
 
     #[test]
     fn test_clean() {
-        let mut manager = media_manager();
-        let pmds = plex_metadata();
-        manager.pmds_to_upsert(&pmds)
-            .iter()
-            .copied()
-            .map(MediaManager::pmd_to_record)
-            .for_each(|record| manager.upsert_record(record));
+        assert!(false);
 
-        manager.clean_library(false);
-        let expected_records = [
-            Record{imdb_id:"tt0021749".into(), title: "in pmds".into(), status: State::Cleaned},
-            Record{imdb_id: "tt0597723552".into(), title: "not in pmds and no magnet".into(), status: State::Cleaned},
-            Record{imdb_id: "tt423090134".into(), title: "not in history".into(), status: State::Missing},
-        ];
-        for (actual, expected) in expected_records.iter().zip(&manager.history.records) {
-            assert_eq!(actual.imdb_id, expected.imdb_id);
-            assert_eq!(actual.title, expected.title);
-            if actual.imdb_id == "tt423090134" {
-                assert!(matches!(actual.status, State::Missing));
-            } else {
-                assert!(matches!(actual.status, State::Cleaned));
-            }
-        }
+        // let mut manager = media_manager();
+        // let pmds = plex_metadata();
+        // manager.get_new_pmds(&pmds)
+        //     .iter()
+        //     .copied()
+        //     .map(MediaManager::pmd_to_record)
+        //     .for_each(|record| manager.upsert_record(record));
+        //
+        // manager.clean_library(false);
+        // let expected_records = [
+        //     Record { imdb_id: "tt0021749".into(), title: "in pmds".into(), status: State::Cleaned },
+        //     Record { imdb_id: "tt0597723552".into(), title: "not in pmds and no magnet".into(), status: State::Cleaned },
+        //     Record { imdb_id: "tt423090134".into(), title: "not in history".into(), status: State::Missing },
+        // ];
+        // for (actual, expected) in expected_records.iter().zip(&manager.history.records) {
+        //     assert_eq!(actual.imdb_id, expected.imdb_id);
+        //     assert_eq!(actual.title, expected.title);
+        //     if actual.imdb_id == "tt423090134" {
+        //         assert!(matches!(actual.status, State::Missing));
+        //     } else {
+        //         assert!(matches!(actual.status, State::Cleaned));
+        //     }
+        // }
     }
 }
